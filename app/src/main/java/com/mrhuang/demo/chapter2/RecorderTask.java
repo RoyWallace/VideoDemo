7a3d908825a105a795ab0c786f50684eb652cdb9
RoyWallace <etonglixianji@gmail.com>
2018-11-20 17:06:14 +0800

    add readme

--- app/src/main/java/com/mrhuang/demo/chapter2/RecorderTask.java
+++ app/src/main/java/com/mrhuang/demo/chapter2/RecorderTask.java
@@ -1,99 +1,92 @@
-package com.mrhuang.demo.chapter2;
+package com.mrhuang.demo.chapter2
 
-import android.media.AudioFormat;
-import android.media.AudioRecord;
-import android.media.MediaRecorder;
-import android.util.Log;
+import android.media.AudioFormat
+import android.media.AudioRecord
+import android.media.MediaRecorder
+import android.util.Log
 
-import com.mrhuang.demo.BaseTask;
+import com.mrhuang.demo.BaseTask
 
-import java.io.BufferedOutputStream;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
+import java.io.BufferedOutputStream
+import java.io.File
+import java.io.FileNotFoundException
+import java.io.FileOutputStream
+import java.io.IOException
+import java.io.OutputStream
 
-public class RecorderTask extends BaseTask<Void, Void, String> {
+class RecorderTask(internal var filePath: String) : BaseTask<Void, Void, String>() {
 
-    AudioRecord audioRecord;
+    internal var audioRecord: AudioRecord
 
-    int minBufferSize;
+    internal var minBufferSize: Int = 0
 
-    int rateInHz = 44100;
-    int channel = AudioFormat.CHANNEL_IN_MONO;
-    int audioEncoding = AudioFormat.ENCODING_PCM_16BIT;
+    internal var rateInHz = 44100
+    internal var channel = AudioFormat.CHANNEL_IN_MONO
+    internal var audioEncoding = AudioFormat.ENCODING_PCM_16BIT
 
-    boolean isRecording = false;
+    internal var isRecording = false
 
-    String filePath;
-
-    public RecorderTask(String filePath) {
-        this.filePath = filePath;
+    internal fun startRecording() {
+        execute()
     }
 
-    void startRecording() {
-        execute();
-    }
+    override fun doInBackground(vararg values: Void): String? {
 
-    @Override
-    protected String doInBackground(Void... values) {
+        minBufferSize = AudioRecord.getMinBufferSize(rateInHz, channel, audioEncoding)
+        audioRecord = AudioRecord(MediaRecorder.AudioSource.MIC, rateInHz, channel, audioEncoding, minBufferSize)
 
-        minBufferSize = AudioRecord.getMinBufferSize(rateInHz, channel, audioEncoding);
-        audioRecord = new AudioRecord(MediaRecorder.AudioSource.MIC, rateInHz, channel, audioEncoding, minBufferSize);
-
-        audioRecord.startRecording();
+        audioRecord.startRecording()
 
 
-        File file = new File(filePath);
+        val file = File(filePath)
 
         //删除已存在的文件
         if (file.exists()) {
-            file.delete();
+            file.delete()
         }
 
         try {
-            file.createNewFile();
-        } catch (IOException e) {
-            e.printStackTrace();
+            file.createNewFile()
+        } catch (e: IOException) {
+            e.printStackTrace()
         }
 
         if (file.exists()) {
             try {
-                OutputStream outputStream = new FileOutputStream(file);
-                BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(outputStream);
+                val outputStream = FileOutputStream(file)
+                val bufferedOutputStream = BufferedOutputStream(outputStream)
 
-                byte[] bytes = new byte[minBufferSize];
-                isRecording = true;
-
+                val bytes = ByteArray(minBufferSize)
+                isRecording = true
+                val current = 0
                 while (isRecording) {
-                    int bufferResult = audioRecord.read(bytes, 0, minBufferSize);
+                    val bufferResult = audioRecord.read(bytes, 0, minBufferSize)
                     if (bufferResult > 0) {
-                        bufferedOutputStream.write(bytes);
-
+                        bufferedOutputStream.write(bytes)
+                        publishProgress()
                     }
                 }
 
-                audioRecord.stop();
-                audioRecord.release();
-                bufferedOutputStream.close();
+                audioRecord.stop()
+                audioRecord.release()
+                bufferedOutputStream.close()
 
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
+            } catch (e: FileNotFoundException) {
+                e.printStackTrace()
+            } catch (e: IOException) {
+                e.printStackTrace()
             }
 
-            PcmToWav.makePCMFileToWAVFile(filePath, filePath.substring(0, filePath.lastIndexOf(".") + 1) + "wav", false);
+            PcmToWav.makePCMFileToWAVFile(filePath, filePath.substring(0, filePath.lastIndexOf(".") + 1) + "wav", false)
 
         } else {
-            Log.i("record", "创建文件失败，文件不存在");
+            Log.i("record", "创建文件失败，文件不存在")
         }
 
-        return null;
+        return null
     }
 
-    void stopRecord() {
-        isRecording = false;
+    internal fun stopRecord() {
+        isRecording = false
     }
 }
